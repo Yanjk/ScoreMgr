7f630fecaa0a85023d9a5fbaf88578102658a34b
Yanjk <yan_jingkai@hotmail.com>
2015-07-30 23:07:40 +0800

    Init

---
+++ ScoreMgr/scoremgr.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+const int STU_MAX = 100;
+const int SUBJ_MAX = 100;
+
+class BaseData;
+class Semester;
+class Student;
+class Subject;
+class Score;
+class Proc;
